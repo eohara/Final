267ad7483052bc626baac9eb0762f02672411fef
Emily O'Hara <eohara@udel.edu>
2018-12-09 16:28:00 -0500

    Intermediate commit

--- FinalExamProject/src/main/java/pkgCore/Retirement.java
+++ FinalExamProject/src/main/java/pkgCore/Retirement.java
@@ -1,5 +1,8 @@
 package pkgCore;
 
+import org.apache.poi.ss.formula.functions.*;
+
+
 public class Retirement {
 
 	private int iYearsToWork;
@@ -9,22 +12,130 @@ public class Retirement {
 	private double dRequiredIncome;
 	private double dMonthlySSI;
 	
+	public Retirement() 
+	{
+		
+	}
+	
+	
+	public Retirement(int iYearsToWork, double dAnnualReturnWorking, int iYearsRetired, double dAnnualReturnRetired,
+			double dRequiredIncome, double dMonthlySSI) {
+		super();
+		this.iYearsToWork = iYearsToWork;
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+		this.iYearsRetired = iYearsRetired;
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+		this.dRequiredIncome = dRequiredIncome;
+		this.dMonthlySSI = dMonthlySSI;
+	}
+
+	
 	//TODO: Build the contructor, getters and setters for the attributes above.
 	
+	
+
+	public int getiYearsToWork() {
+		return iYearsToWork;
+	}
+
+
+	public void setiYearsToWork(int iYearsToWork) {
+		this.iYearsToWork = iYearsToWork;
+	}
+
+	
+	public int getiYearsRetired() {
+		return iYearsRetired;
+	}
+
+
+	public void setiYearsRetired(int iYearsRetired) {
+		this.iYearsRetired = iYearsRetired;
+	}
+
+	public double getdAnnualReturnWorking() {
+		return dAnnualReturnWorking;
+	}
+
+
+	public void setdAnnualReturnWorking(double dAnnualReturnWorking) {
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+	}
+
+
+	public double getdAnnualReturnRetired() {
+		return dAnnualReturnRetired;
+	}
+
+
+	public void setdAnnualReturnRetired(double dAnnualReturnRetired) {
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+	}
+
+
+	public double getdRequiredIncome() {
+		return dRequiredIncome;
+	}
+
+
+	public void setdRequiredIncome(double dRequiredIncome) {
+		this.dRequiredIncome = dRequiredIncome;
+	}
+
+
+	public double getdMonthlySSI() {
+		return dMonthlySSI;
+	}
+
+
+	public void setdMonthlySSI(double dMonthlySSI) {
+		this.dMonthlySSI = dMonthlySSI;
+	}
+
+
 	public double AmountToSave()
 	{
 		//TODO: Determine the amount to save each month based on TotalAmountSaved, YearsToWork
 		//		and Annual return while working
 		
+		/*
+		double r =  dAnnualReturnWorking / 12;
+		int n = iYearsToWork * 12;
+		double p = TotalAmountSaved;
+		double f = 0;
+		boolean t = false;
+		*/
 		
-		return 0;
+		//Retirement rtm = new Retirement();
+		
+		
+		//double iTotalSaved = rtm.TotalAmountSaved();
+		
+		
+		
+		double PMT = (int)(FinanceLib.pmt(dAnnualReturnWorking / 12, iYearsToWork * 12, 0, TotalAmountSaved(), false)*100);
+
+		//System.out.println(PMT);
+		
+		return -1*PMT/100;
 	}
+	
 	
 	public double TotalAmountSaved()
 	{
 		//	TODO: Determine amount to be saved based on Monthly SSI, Required Income, Annual return during retirement
 		//		and number of years retired.
 		//
-		return 0;
+		
+		//Retirement rtm = new Retirement();
+		//double iPayment = rtm.AmountToSave();
+		
+		
+		double PV = (int)(FinanceLib.pv(dAnnualReturnRetired / 12, iYearsRetired * 12 , dMonthlySSI - dRequiredIncome, 0, false)*100);
+
+		//System.out.println(PV);
+
+		return (double) PV/100;
 	}
+	
 }
---
+++ FinalExamProject/src/test/java/pkgEmpty/RetirementTest.java
@@ -0,0 +1,57 @@
+package pkgEmpty;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pkgCore.Retirement;
+
+public class RetirementTest{
+	
+	@Test
+	public void RetirementPMT() {
+		
+		Retirement rtm = new Retirement();
+		
+		/*double r =  0.07;
+		int n = 40;
+		double y = rtm.TotalAmountSaved();
+		double f = 0;
+		boolean t = false;
+		
+		//double ExpectedSaved = 1454485.55;
+		
+		*/
+		rtm.setiYearsToWork(40);
+		rtm.setdAnnualReturnRetired(0.02);
+		rtm.setdAnnualReturnWorking(0.07);
+		rtm.setdRequiredIncome(10000);
+		rtm.setdMonthlySSI(2642);
+	    rtm.setiYearsRetired(20);	
+		
+		System.out.println(Math.abs(rtm.AmountToSave()));
+
+		
+		assertEquals(rtm.AmountToSave(),554.13,0.01);
+	}
+	
+	@Test
+	public void RetirementPV() {
+		
+		Retirement rtm = new Retirement();
+		
+		rtm.setdAnnualReturnRetired(0.02);
+		rtm.setdRequiredIncome(10000);
+		rtm.setdMonthlySSI(2642);
+		rtm.setiYearsRetired(20);
+		
+		System.out.println(Math.abs(rtm.TotalAmountSaved()));
+		
+		assertEquals(rtm.TotalAmountSaved(),1454485.55,0.03);
+	}
+	
+	
+	
+	
+	
+}
